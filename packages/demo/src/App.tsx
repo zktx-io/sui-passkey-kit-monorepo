3879dad4d0216916ac4e17f8d6211bba442fbf8a
Lim Jet <57783762+daoauth@users.noreply.github.com>
2025-03-12 23:01:52 +0900

    update walrus-clip

--- packages/demo/src/App.tsx
+++ packages/demo/src/App.tsx
@@ -4,10 +4,13 @@ import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
 import { getFullnodeUrl } from '@mysten/sui/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { SuiPasskey } from '@zktx.io/sui-passkey-kit';
+import { WalrusWallet } from '@zktx.io/walrus-wallet';
 
 import './App.css';
 import '@mysten/dapp-kit/dist/index.css';
-import { Home, NETWORK } from './pages/Home';
+import { Home } from './pages/Home';
+import { ICON, NETWORK, WALLET_NAME } from './config';
+import { enqueueSnackbar } from 'notistack';
 
 const router = createBrowserRouter([
   {
@@ -21,22 +24,38 @@ function App() {
     NETWORK,
   );
   return (
-    <SuiPasskey network={activeNetwork}>
-      <SuiClientProvider
-        networks={{
-          testnet: { url: getFullnodeUrl('testnet') },
-          devnet: { url: getFullnodeUrl('devnet') },
-        }}
-        defaultNetwork={activeNetwork as 'testnet' | 'devnet'}
-        onNetworkChange={(network) => {
-          setActiveNetwork(network);
-        }}
-      >
-        <WalletProvider autoConnect>
-          <RouterProvider router={router} />
-        </WalletProvider>
-      </SuiClientProvider>
-    </SuiPasskey>
+    <WalrusWallet
+      name={WALLET_NAME}
+      icon={ICON}
+      network={activeNetwork}
+      onEvent={(notification) => {
+        enqueueSnackbar(notification.message, {
+          variant: notification.variant,
+          style: {
+            whiteSpace: 'nowrap',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          },
+        });
+      }}
+    >
+      <SuiPasskey network={activeNetwork}>
+        <SuiClientProvider
+          networks={{
+            testnet: { url: getFullnodeUrl('testnet') },
+            devnet: { url: getFullnodeUrl('devnet') },
+          }}
+          defaultNetwork={activeNetwork as 'testnet' | 'devnet'}
+          onNetworkChange={(network) => {
+            setActiveNetwork(network);
+          }}
+        >
+          <WalletProvider autoConnect>
+            <RouterProvider router={router} />
+          </WalletProvider>
+        </SuiClientProvider>
+      </SuiPasskey>
+    </WalrusWallet>
   );
 }
 
